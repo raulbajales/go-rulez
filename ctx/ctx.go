52bb4007bc4137d0a775d8aff5a3c7c77c86ffe6
raulbajales <raul.bajales@gmail.com>
2016-05-06 19:26:06 -0300

    Basic interfaces defined

---
+++ ctx/ctx.go
@@ -0,0 +1,11 @@
+package ctx
+
+type m map[string]interface{}
+
+type In struct {
+	m
+}
+
+type Out struct {
+	m
+}
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"./ctx"
+)
+
+type Rule interface {
+	When() (bool, error)
+	Then() error
+}
+
+type Engine interface {
+	AddListener(l Listener)
+	AddRule(Rule)
+	Run(in ctx.In) (ctx.Out, error)
+}
+
+type Listener interface {
+	Before() error
+	After() error
+	Success() error
+	Failure() error
+}
